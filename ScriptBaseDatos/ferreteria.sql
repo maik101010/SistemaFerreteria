14e7053c852311edd3c63a78c0f0dd7d10d40606
michael eduardo garcia <maiksoft10@gmail.com>
2018-11-24 16:42:15 -0500

    Se agrega el script bd de la primera version

--- ScriptBaseDatos/ferreteria.sql
+++ ScriptBaseDatos/ferreteria.sql
@@ -28,12 +28,26 @@ CREATE TABLE IF NOT EXISTS `producto` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla ferreteria.producto: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla ferreteria.producto: ~2 rows (aproximadamente)
 /*!40000 ALTER TABLE `producto` DISABLE KEYS */;
 INSERT INTO `producto` (`id`, `referencia`, `nombre`, `marca`, `valor`, `cantidadStock`, `estado`) VALUES
-	(2, '123ja', 'Cacao', 'Clorox', 5000, 2, '0'),
-	(3, '123j1', 'Agua en botella', 'Brisa', 2000, 5, '1');
+	(2, '123ja', 'Sprite', 'Coca Cola', 5000, 5, '1'),
+	(3, '123j1', 'Agua en botella', 'Brisa', 2000, 20, '1');
 /*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+
+-- Volcando estructura para tabla ferreteria.usuario
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(200) DEFAULT NULL,
+  `contrasenia` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Volcando datos para la tabla ferreteria.usuario: ~1 rows (aproximadamente)
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` (`id`, `usuario`, `contrasenia`) VALUES
+	(1, 'admin', 'nueva');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
 
 -- Volcando estructura para tabla ferreteria.venta
 CREATE TABLE IF NOT EXISTS `venta` (
@@ -46,13 +60,31 @@ CREATE TABLE IF NOT EXISTS `venta` (
   CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
--- Volcando datos para la tabla ferreteria.venta: ~0 rows (aproximadamente)
+-- Volcando datos para la tabla ferreteria.venta: ~3 rows (aproximadamente)
 /*!40000 ALTER TABLE `venta` DISABLE KEYS */;
 INSERT INTO `venta` (`id`, `cantidadSolicitada`, `fecha`, `producto_id`) VALUES
 	(1, 2, '2018-11-21', 3),
 	(2, 4, '2018-11-21', 3),
 	(3, 1, '2018-11-21', 3);
 /*!40000 ALTER TABLE `venta` ENABLE KEYS */;
+
+-- Volcando estructura para tabla ferreteria.venta_temp
+CREATE TABLE IF NOT EXISTS `venta_temp` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cantidadSolicitada` int(11) DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `producto_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `venta_ibfk_2` (`producto_id`),
+  CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`producto_id`) REFERENCES `producto` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+-- Volcando datos para la tabla ferreteria.venta_temp: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `venta_temp` DISABLE KEYS */;
+INSERT INTO `venta_temp` (`id`, `cantidadSolicitada`, `fecha`, `producto_id`) VALUES
+	(1, 5, '2018-11-24', 2),
+	(2, 4, '2018-11-24', 3);
+/*!40000 ALTER TABLE `venta_temp` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
